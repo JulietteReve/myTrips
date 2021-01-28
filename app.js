693e829bff2d13cbae232fda70e4c903f7d71a7f
juliette reverdy <juliettereverdy@MBP-de-juliette.concorde-pereire.loc>
2021-01-28 11:09:33 +0100

    reorga

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var models = require('./routes/index')
+var connexion = require('./models/connexion')
 
 var app = express();
 
