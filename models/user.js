bb0f47e973ca55b94dbfd4b501d4dc212ee96c22
olivblad <obladier@gmail.com>
2021-01-28 11:39:32 +0100

    ajout express-session & userModel

---
+++ models/user.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  lastname: {
+    type: String,
+    required: true,
+  },
+  firstname: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  journeys: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "journeys",
+  },
+});
+
+const userModel = mongoose.model("users", userSchema);
+
+module.exports = userModel;
