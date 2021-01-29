ba00fdfdf94159c8865443f2c9471cb112f51478
olivblad <obladier@gmail.com>
2021-01-29 14:03:58 +0100

    fixing bug sur autocompletion

--- public/js/script.js
+++ public/js/script.js
@@ -8,7 +8,7 @@ departure.attr("autocomplete", "off");
 arrival.attr("autocomplete", "off");
 
 //Function to search and filter your research and match with db
-const searchCities = async (searchText, matchInp) => {
+const searchCities = async (searchText, matchContainer, searchInput) => {
   const res = await fetch("http://localhost:3000/tickets/get-departures");
   const cities = await res.json();
   let matches = cities.filter((city) => {
@@ -18,13 +18,13 @@ const searchCities = async (searchText, matchInp) => {
 
   if (searchText.length === 0) {
     matches = [];
-    matchInp.html("");
+    matchContainer.html("");
   }
-  passMatchToHtml(matches, matchInp);
+  passMatchToHtml(matches, matchContainer, searchInput);
 };
 
-//Function to pass content to HTML
-const passMatchToHtml = (matches, matchInp) => {
+//Function to pass content from search input to HTML (match list)
+const passMatchToHtml = (matches, matchContainer, searchInput) => {
   if (matches.length > 0) {
     const html = matches
       .map(
@@ -32,29 +32,24 @@ const passMatchToHtml = (matches, matchInp) => {
           `<div class="list-group-item"><p class="p-0 m-0">${match}</p></div>`
       )
       .join("");
-    matchInp.html(html);
+    matchContainer.html(html);
+    matchContainer.children().each(function () {
+      $(this).on("click", function () {
+        const city = $(this).children().text();
+        passCityToHtml(city, searchInput, $(this));
+      });
+    });
   }
 };
 
-const passCityToHtml = (city, searchInp, element) => {
+//Function to pass content from selected City to HTML (search input)
+const passCityToHtml = (city, searchInput, element) => {
   if (city.length > 0) {
-    searchInp.val(`${city}`);
-    element.html("");
+    searchInput.val(`${city}`);
+    element.parent().html("");
   }
 };
 
 //EventListeners to implement autocompletion
-departure.on("input", (e) => searchCities(e.target.value, matchD));
-arrival.on("input", (e) => searchCities(e.target.value, matchA));
-matchD.each(function () {
-  $(this).on("click", function () {
-    const city = $(this).children().children().text();
-    passCityToHtml(city, departure, $(this));
-  });
-});
-matchA.each(function () {
-  $(this).on("click", function () {
-    const city = $(this).children().children().text();
-    passCityToHtml(city, arrival, $(this));
-  });
-});
+departure.on("input", (e) => searchCities(e.target.value, matchD, departure));
+arrival.on("input", (e) => searchCities(e.target.value, matchA, arrival));
